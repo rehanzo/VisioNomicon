6f5c2760fb5f836209c3cb3ea45b385dad6b6326
Rehan <rehanalirana@tuta.io>
2023-12-28 16:20:00 -0500

    adds ability to execute mapping

--- args_handler.py
+++ args_handler.py
@@ -4,16 +4,29 @@ import os
 def parse_cli_args():
     # TODO: Better help and description messages
     parser = argparse.ArgumentParser(description='Process some CLI arguments.')
-    parser.add_argument('input_files', type=str, nargs='+', help='The input file paths')
+    parser.add_argument('input_files', type=str, nargs='*', help='The input file paths')
     parser.add_argument('-c', type=str, help='Mapping file to create')
-    parser.add_argument('-x', type=str, help='Execute on given mapping')
+    parser.add_argument('-x', type=str, nargs='?', help='Execute on given mapping', const='')
+    # if flag with value, equals value
+    # if flag with no value, equals default value
+    # if flag not used, equals None
     
     args = parser.parse_args()
 
+    # TODO: Argument pairings, etc.
+    # input files by itself should create mapping at default location
+    # input files with -c creates mapping at given location
+        # -c with nothing should error
+        # -c without input files should error
+    # -x should be by itself.....unless?
+        # maybe allow usage with input_files and -c for a one and done sort of thing?
     if args.input_files is not None:
         args.input_files = [os.path.abspath(path) for path in args.input_files]
 
-    if args.c is not None:
+    if args.c is not None and args.c != '':
         args.c = os.path.abspath(args.c)
+
+    if args.x is not None and args.x != '':
+        args.x = os.path.abspath(args.x)
     
     return args
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import json
 import copy
-from args_handler import parse_cli_args
-from gpt import image_to_name, name_validation
+import glob
+from args_handler import *
+from gpt import *
 from datetime import datetime
 
 DATA_DIR = ""
@@ -17,22 +18,55 @@ def main():
 
   args = parse_cli_args()
 
-  og_filepaths = args.input_files
-  new_filepaths: list[str] = generate_mapping(og_filepaths)
+  # if creating mapping
+  if len(args.input_files) != 0:
+    og_filepaths = args.input_files
+    new_filepaths: list[str] = generate_mapping(og_filepaths)
 
-  # have new and old, put them together into a json and save
-  save_mapping(args, new_filepaths)
+    # have new and old, put them together into a json and save
+    save_mapping(args, new_filepaths)
+
+  # if executing
+  if args.x is not None:
+    mapping_fp = get_mapping_name(args.x)
+
+    og_filepaths: list[str] = []
+    new_filepaths: list[str] = []
+
+    with open(mapping_fp) as f:
+      data = json.load(f)
+      og_filepaths += list(data.keys())
+      new_filepaths += list(data.values())
+
+
+    for i in range(len(og_filepaths)):
+      print("renaming {} to {}".format(og_filepaths[i], new_filepaths[i]))
+      os.rename(og_filepaths[i], new_filepaths[i])
+
+def get_mapping_name(cli_fp: str):
+  if cli_fp != '':
+    return cli_fp
+  else:
+    # Join the directory with the file pattern
+    file_pattern = os.path.join(DATA_DIR, 'mapping-*.json')
+
+    # Get list of files matching the file pattern
+    files = glob.glob(file_pattern)
+
+    # Sort files by creation time and get the last one
+    mapping = max(files, key=os.path.getctime)
+    return mapping
 
 def save_mapping(args, new_filepaths: list[str]):
   og_filepaths: list[str] = args.input_files
   data = dict(zip(og_filepaths, new_filepaths))
   # data ready to dump, need to get mapping filename
-  mapping_filename = get_mapping_name(args)
+  mapping_filename = generate_mapping_name(args)
 
   with open(mapping_filename, 'w') as file:
     json.dump(data, file, indent=4)
 
-def get_mapping_name(args) -> str:
+def generate_mapping_name(args) -> str:
   return args.c if args.c is not None else DATA_DIR + datetime.now().strftime("mapping-%Y-%m-%d-%H-%M-%S.json")
   
 def generate_mapping(og_filepaths: list[str]) -> list[str]:

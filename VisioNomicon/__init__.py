78f3241cc9b8d3e6b5626dcd4f93f6955dfaa573
Rehan <github@rehanzo.com>
2024-01-05 18:53:21 -0500

    bump version

--- VisioNomicon/__init__.py
+++ VisioNomicon/__init__.py
@@ -4,5 +4,5 @@ VisioNomicon.
 A utility leveraging GPT-4V for image file renaming based on content. 
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = 'Rehan Rana'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VisioNomicon',
-    version='0.1.1',
+    version='0.1.2',
     author='Rehan Rana',
     author_email='visionomicon@rehanzo.com',
     description='A utility leveraging GPT-4V for image file renaming based on content.',

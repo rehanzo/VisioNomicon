0c20d2bc372b6b5ace8e631548eca07009e80e90
Rehan <github@rehanzo.com>
2024-05-09 21:00:46 -0400

    bump version

--- setup.py
+++ setup.py
@@ -1,33 +1,32 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='VisioNomicon',
-    version='0.1.2',
-    author='Rehan Rana',
-    author_email='visionomicon@rehanzo.com',
-    description='A utility leveraging GPT-4V for image file renaming based on content.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/rehanzo/visionomicon',
+    name="VisioNomicon",
+    version="0.1.3",
+    author="Rehan Rana",
+    author_email="visionomicon@rehanzo.com",
+    description="A utility leveraging GPT-4V for image file renaming based on content.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rehanzo/visionomicon",
     packages=find_packages(),
     install_requires=[
-        'requests',
-        'openai',
+        "requests",
+        "openai",
     ],
     entry_points={
-        'console_scripts': [
-            'VisioNomicon=VisioNomicon.main:main',
+        "console_scripts": [
+            "VisioNomicon=VisioNomicon.main:main",
         ],
     },
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Utilities',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
     ],
-    keywords='gpt-4 gpt-4v openai renaming images',
+    keywords="gpt-4 gpt-4v openai renaming images",
 )
-
